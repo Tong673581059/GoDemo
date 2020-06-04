e9c75c48bee07046f09074f08a1d644e04c9a210
zhuxuetong <zhuxuetong@ihicon.com>
2020-06-04 17:34:06 +0800

    1、接口txt数据处理

--- util/StringUtil.go
+++ util/StringUtil.go
@@ -1,12 +1,15 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func StrToJson(jsonStr string) map[string]interface{} {
 
 	var dat map[string]interface{}
-	if err := json.Unmarshal([]byte(jsonStr), &dat); err == nil {
-
+	if err := json.Unmarshal([]byte(jsonStr), &dat); err != nil {
+		fmt.Println(err)
 	}
 	return dat
 }
